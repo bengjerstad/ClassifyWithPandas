ea913caea206f3211b6f64862850a484b8d014c9
bengjerstad <bengjerstad@gmail.com>
2019-12-18 10:14:01 -0500

    Update and rename CleanUp.py to DataCleanup.py
    Make this an importable module

--- DataCleanup.py
+++ DataCleanup.py
@@ -2,54 +2,49 @@ import json
 import re
 import pandas as pd
 
-#Category Transforms
-#Goal: Fix or Drop Categories that have been categorized wrong without changeing the orignal data
-#save Transform into an excel doc with a seprate page for each kind of transform.
-#Each sheet need 2 cols, Name and Action
+# Category Transforms
+# Goal: Fix or Drop Categories that have been categorized wrong without changeing the orignal data
+# save Transform into an excel doc with a seprate page for each kind of transform.
+# Each sheet need 2 cols, Name and Action
+TranslationLookUp = pd.read_excel('Transforms.xlsx',sheet_name="Types",index_col="Name")['Action'].to_dict()
+WOTranslationLookUp = pd.read_excel('Transforms.xlsx',sheet_name="WOs",index_col="Name")['Action'].to_dict()
 
-#Transforms Type
-#Fix or Drop Categories based on the current Category
-print("Progress: Transforms Type")
-def Translate(x):
+# Transforms Type
+# Fix or Drop Categories based on the current Category
+def TranslateType(x):
     CurrentType = x['TYPE']
     if CurrentType in list(TranslationLookUp.keys()):
         x['TYPE'] = TranslationLookUp[CurrentType]
     if x['TYPE'] != 'Drop' and type(x['TASK']) == type(""):
         return x
-		
-TypeTranslation = pd.read_excel('Transforms.xlsx',sheet_name="Types",index_col="Name")
-TranslationLookUp = TypeTranslation['Action'].to_dict()
-df = df.apply(Translate,axis=1).dropna(how='all')
-print(len(df))
 
-#Transforms WOs
-#Fix or Drop Categories based on the Indvle ID of the row. 
-print("Progress: Transforms WOs")
-def Translate(x):
+# Transforms WOs
+# Fix or Drop Categories based on the Indvle ID of the row. 
+def TranslateWO(x):
     CurrentWO = x.WO_NUM
-    if CurrentWO in list(TranslationLookUp.keys()):
-        x['TYPE'] = TranslationLookUp[CurrentWO]
+    if CurrentWO in list(WOTranslationLookUp.keys()):
+        x['TYPE'] = WOTranslationLookUp[CurrentWO]
     if x['TYPE'] != 'Drop' and type(x['TYPE']) == type(""):
         return x
-		
-WOTranslation = pd.read_excel('Transforms.xlsx',sheet_name="WOs",index_col="Name")
-TranslationLookUp = WOTranslation['Action'].to_dict()
-df = df.apply(Translate,axis=1).dropna(how='all')
-print(len(df))
 
-#Word Level Clean UP
-#Remove Names
-with open("namelists.json", "r") as read_file:
-    NameTranslation = json.load(read_file)
-   
-def removenames(x)
-    #Remove the First Names
+# Word Level Clean UP
+# Remove Names
+def removenames(x):
+    with open("namelists.json", "r") as read_file:
+        NameTranslation = json.load(read_file)
     namelist = NameTranslation['first']
     for name in namelist:
         if type(name) is str:
             if name.lower()+" " in x:
                 x = x.replace(name.lower(),'RFirstName')
-    #Remove the Last Names
+    namelist = NameTranslation['last']
+    for name in namelist:
+        if type(name) is str:
+            if " "+name.lower() in x:
+                x = x.replace(name.lower(),'RLastName')
+    if '[FName] [LName]' in x:
+        x = x.replace('RFirstName RLastName','RFullName')
+    return x
     namelist = NameTranslation['last']
     for name in namelist:
         if type(name) is str:
@@ -80,9 +75,3 @@ def cleanup(x):
     x = re.sub(r'\b\d{10}?\b', 'TenDig', x)
     x = x.replace(' - ',' ')
     return x
-
-print("Progress: CleanUp")
-df['TASKClean'] = df.TASK.apply(cleanup)
-
-#Export Cleaned Data to review the transformed data results
-df.to_csv("CleanedData.csv")
